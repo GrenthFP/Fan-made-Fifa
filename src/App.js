01e83a7958fb043e4182978df13c59f558deeb7b
Grenth <alduin312@gmail.com>
2020-03-26 21:10:57 +0200

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import ReactModal from "react-modal";
+const player_pool = ["NIKE.png", "G.png", "ceco4.png"];
+const player_pool_small = ["NIKE2.png", "G3.png", "ceco4_small.png"];
+const paralel_pool = [
+  "position_niki.png",
+  "position_G.png",
+  "position_ceco.png"
+];
 
+const ids = [
+  "card1",
+  "card2",
+  "card3",
+  "card4",
+  "card5",
+  "card6",
+  "card7",
+  "card8",
+  "card9",
+  "card10",
+  "card11"
+];
 function App() {
   const [effects, set_effects] = useState([]);
   const [stat, set_stat] = useState(false);
   const [draft, set_draft] = useState(false);
   const [draft2, set_draft2] = useState(false);
   const [player, set_player] = useState("");
-  const player_pool = ["NIKE.png", "G.png", "ceco4.png"];
-  const player_pool_small = ["NIKE2.png", "G3.png", "ceco4_small.png"];
-  const [small_players, set_small_players] = useState([]);
-  const [paralel, set_paralel] = useState([]);
-  const paralel_pool = [
-    "position_niki.png",
-    "position_G.png",
-    "position_ceco.png"
-  ];
-  const [chosen_player, set_chosen_player] = useState("");
   const [player_positions, set_player_positions] = useState([
     "empty card.png",
     "empty card.png",
@@ -32,19 +42,10 @@ function App() {
     "empty card.png",
     "empty card.png"
   ]);
-  const ids = [
-    "card1",
-    "card2",
-    "card3",
-    "card4",
-    "card5",
-    "card6",
-    "card7",
-    "card8",
-    "card9",
-    "card10",
-    "card11"
-  ];
+  const [small_players, set_small_players] = useState([]);
+  const [paralel, set_paralel] = useState([]);
+
+  const [chosen_player, set_chosen_player] = useState("");
 
   const handleAfterOpenFunc = () => {
     let chance = Math.floor(Math.random() * 3);
@@ -84,10 +85,16 @@ function App() {
     set_chosen_player(i);
   };
   const changer = i => {
-    let test_array = player_positions;
-    test_array[chosen_player] = paralel[i];
-    set_player_positions(test_array);
     set_draft2(false);
+    if (player_positions[chosen_player] == "empty card.png") {
+      player_positions[chosen_player] = paralel[i];
+      set_player_positions(player_positions);
+      console.log(i);
+      paralel.splice(i, 1);
+      set_paralel(paralel);
+      small_players.splice(i, 1);
+      set_small_players(small_players);
+    }
   };
 
   return (
@@ -148,16 +155,19 @@ function App() {
         contentLabel={"Example Modal"}
       >
         <button onClick={close_button_three}>close</button>
-        {console.log(small_players)}
         <div id="smallpl">
-          {small_players.map((small_players, i) => (
-            <img key={i} src={small_players} onClick={() => changer(i)}></img>
+          {small_players.map((small_player, i) => (
+            <img
+              key={small_player}
+              src={small_player}
+              onClick={() => changer(i)}
+            ></img>
           ))}
         </div>
       </ReactModal>
       {player_positions.map((player_positions, i) => (
         <img
-          key={i}
+          key={player_positions}
           onClick={() => change(i)}
           src={player_positions}
           id={ids[i]}
